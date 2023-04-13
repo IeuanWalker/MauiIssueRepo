b53ebd26e4010df07b5e9eaaaa00e0faeccf2a5c
Ieuan Walker <ieuan.walker007@gmail.com>
2023-04-13 17:36:58 +0100

    Add project files.

---
+++ App/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using App.ViewModels;
+
+namespace App
+{
+    public partial class MainPage : ContentPage
+    {
+        HomeViewModel vm = new();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = vm;
+            LogCarousel.Position = 30;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            LogCarousel.Position = 30;
+        }
+    }
+}
---
+++ App/ViewModels/HomeViewModel.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace App.ViewModels
+{
+    public partial class HomeViewModel : BaseViewModel
+    {
+        public HomeViewModel()
+        {
+            DateTime end = DateTime.Now.AddDays(1);
+            DateTime start = DateTime.Now.AddDays(-30);
+            Logs = new ObservableCollection<TestModel>(Enumerable.Range(0, 1 + end.Subtract(start).Days)
+                .Select(offset => start.AddDays(offset))
+                .Select(x => new TestModel
+                {
+                    Date = x
+                }));
+
+            SelectedLog = Logs.Last();
+        }
+
+        [ObservableProperty]
+        ObservableCollection<TestModel> _logs = new();
+
+        [ObservableProperty]
+        TestModel _selectedLog = null!;
+    }
+
+    public class TestModel
+    {
+        public DateTime Date { get; set; }
+        public string DateDisplayTest
+        {
+            get {
+                if (Date.Date.Equals(DateTime.Now.Date))
+                {
+                    return "Today";
+                }
+                else if (Date.Date.Equals(DateTime.Now.AddDays(-1).Date))
+                {
+                    return "Yesterday";
+                }
+
+                return Date.ToString("d, MMMM yyyy");
+            }
+        }
+    }
+}
